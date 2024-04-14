aca55c14e1a3beb239754a225a00782ac60e13b5
Mo Hajbabaei <h9edev@gmail.com>
2024-04-14 04:38:05 +0400

    feat: add test file upload

--- test_app/main.go
+++ test_app/main.go
@@ -1,25 +1,61 @@
 package main
 
 import (
+	"context"
 	"log"
+	"net/http"
+	"time"
 
 	"github.com/minio/minio-go/v7"
 	"github.com/minio/minio-go/v7/pkg/credentials"
 )
 
 func main() {
+	ctx := context.Background()
 	endpoint := "localhost:9000"
 	accessKeyID := "minioadmin"
 	secretAccessKey := "minioadmin"
 	useSSL := false
 
+	customTransport := &http.Transport{
+		TLSClientConfig:    nil,
+		MaxIdleConns:       100,
+		IdleConnTimeout:    90 * time.Second,
+		DisableCompression: true,
+	}
+
 	minioClient, err := minio.New(endpoint, &minio.Options{
-		Creds:  credentials.NewStaticV4(accessKeyID, secretAccessKey, ""),
-		Secure: useSSL,
+		Creds:     credentials.NewStaticV4(accessKeyID, secretAccessKey, ""),
+		Secure:    useSSL,
+		Transport: customTransport,
 	})
 	if err != nil {
 		log.Fatalln(err)
 	}
 
-	log.Printf("%#v\n", minioClient)
+	bucketName := "testbucket"
+	location := "us-east-1"
+
+	err = minioClient.MakeBucket(ctx, bucketName, minio.MakeBucketOptions{Region: location})
+	if err != nil {
+		exists, errBucketExists := minioClient.BucketExists(ctx, bucketName)
+		if errBucketExists == nil && exists {
+			log.Printf("We already own %s\n", bucketName)
+		} else {
+			log.Fatalln(err)
+		}
+	} else {
+		log.Printf("Successfully created %s\n", bucketName)
+	}
+
+	objectName := "testdata"
+	filePath := "./test.txt"
+	contentType := "application/octet-stream"
+
+	info, err := minioClient.FPutObject(ctx, bucketName, objectName, filePath, minio.PutObjectOptions{ContentType: contentType})
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	log.Printf("Successfully uploaded %s of size %d\n", objectName, info.Size)
 }
